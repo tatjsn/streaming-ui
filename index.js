768bb79dcbf72af7a5c665d90e239521c0ccd224
Taz Jackson <tatjsn@users.noreply.github.com>
2022-04-07 22:23:23 +0100

    Use realistic content

--- index.js
+++ index.js
@@ -21,47 +21,52 @@ app.get('/', async (req, res) => {
   res.setHeader('Transfer-Encoding', 'chunked');
 
   res.write(html`<!DOCTYPE html>
-    <html>
-    <body>
-      <h1>Hello from slow content</h1>
-      <p>Main content</p>
-      <p>Main content</p>
-      <lol-suspense match="^widget1">
-        <p>Loading...</p>
-      </lol-suspense>
-      <lol-suspense match="^widget2">
-        <p>Loading...</p>
-      </lol-suspense>
-      <lol-suspense match="^widget3">
-        <p>Loading...</p>
-      </lol-suspense>
-      <p>Main content</p>
-      <p>End of contentful part</p>
-      <script src="/build/index.js"></script>
+<html>
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" type="text/css" href="/build/index.css" />
+</head>
+<body>
+<div class="container">
+  <div class="header">
+    <div>Header</div>
+    <div></div>
+    <div>
+      Stocks:
+      APPL:<lol-suspense match="appl"><div class="loading"></div></lol-suspense> |
+      GOOG:<lol-suspense match="goog"><div class="loading"></div></lol-suspense>
+    </div>
+  </div>
+  <div class="content">
+    <div class="box">
+      <h1>Main Content</h1>
+      <p>CSS Grid Layout is a two-dimensional layout system for the web. It lets you lay content out in rows and columns. It has many features that make building complex layouts straightforward. This article will explain all you need to know to get started with page layout.</p>
+      <p>CSS Grid Layout is a two-dimensional layout system for the web. It lets you lay content out in rows and columns. It has many features that make building complex layouts straightforward. This article will explain all you need to know to get started with page layout.</p>
+      <p>CSS Grid Layout is a two-dimensional layout system for the web. It lets you lay content out in rows and columns. It has many features that make building complex layouts straightforward. This article will explain all you need to know to get started with page layout.</p>
+    </div>
+  </div>
+  <div class="footer">Footer (c) 2022</div>
+</div>
+<script src="/build/index.js"></script>
   `);
 
   // Load slow dependencies
   await Promise.all([
-    new Promise(r => setTimeout(r, 1000)).then(() => {
-      res.write(html`<template id="widget1">
-        <p>Widget 1 loaded</p>
-      </template>`);
-    }),
     new Promise(r => setTimeout(r, 2000)).then(() => {
-      res.write(html`<template id="widget2">
-        <p>Widget 2 loaded</p>
+      res.write(html`<template id="appl">
+        <span>172.14 &#10548;&#65039;</span>
       </template>`);
     }),
-    new Promise(r => setTimeout(r, 3000)).then(() => {
-      res.write(html`<template id="widget3">
-        <p>Widget 3 loaded</p>
+    new Promise(r => setTimeout(r, 4000)).then(() => {
+      res.write(html`<template id="goog">
+        <span>2700.00 &#10549;&#65039;</span>
       </template>`);
     }),
   ]);
 
   res.write(html`
-     </body>
-   </html>
+</body>
+</html>
   `);
   res.end();
 });
